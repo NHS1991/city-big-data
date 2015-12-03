8ae0bec9c8187ef69dc82740b3f5d05b23c9e854
Robin Ricard <ricard.robin@gmail.com>
2015-12-03 09:40:04 +0100

    Remove clojure hadoop project
    We're going to switch to spark

--- project.clj
+++
@@ -1,4 +0,0 @@
-(defproject city-big-data "1.0"
-  :description "Hadoop algorithms for a city's events big data analysis"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clojure-hadoop "1.4.4"]])
